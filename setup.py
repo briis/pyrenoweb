894c1168b7ff82857bc3f2316a32d46b3c656b85
Bjarne Riis <bjarne@briis.com>
2020-11-03 07:33:25 +0100

    Bumped to 0.1.15

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="pyrenoweb",
     packages=["pyrenoweb"],
-    version="0.1.14",
+    version="0.1.15",
     license="MIT",
     description="Python Wrapper for RenoWeb Garbage System API",
     long_description=" ".join(
