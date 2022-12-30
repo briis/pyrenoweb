11bcdfdd9d498289eb0c9fbbdb51ab76e84dae19
Bjarne Riis <bjarne@briis.com>
2022-12-30 09:47:27 +0100

    Bump to 1.0.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="pyrenoweb",
     packages=["pyrenoweb"],
-    version="1.0.6",
+    version="1.0.7",
     license="MIT",
     description="Python Wrapper for RenoWeb Garbage System API",
     long_description=" ".join(
