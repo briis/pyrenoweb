2b08764ced1ff43752eef6826b5b646dd91dcc5b
Bjarne Riis <bjarne@briis.com>
2024-03-22 17:12:53 +0100

    Bump to 2.0.15

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md") as fh:
 
 setuptools.setup(
     name="pyrenoweb",
-    version="2.0.14",
+    version="2.0.15",
     author="briis",
     author_email="bjarne@briis.com",
     description="Gets garbage collection data from RenoWeb",
